89f5af58dc100e485615ed6b34430cf7cd7c9376
Channaka Gunawardhane <channakagunawardana@gmail.com>
2023-12-05 19:28:37 +0530

    20231205-1928

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -3,4 +3,22 @@ import 'package:flutter_bloc_todo/models/todo_model.dart';
 
 class TodoCubit extends Cubit<List<Todo>> {
   TodoCubit() : super([]);
+
+  void addTodo(String title) {
+    if (title.isEmpty) {
+      addError('Text field empty, add some text');
+      return;
+    }
+    final todo = Todo(
+      name: title,
+      createdAt: DateTime.now(),
+    );
+
+    emit([...state, todo]);
+  }
+
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print(error);
+  }
 }
